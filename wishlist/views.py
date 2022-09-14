f12281b94d84ad8df5710312b19afcb90016d2ce
hayfa_najma <hayfanajmaa22@gmail.com>
2022-09-14 22:27:08 +0700

    lab 2 selesai

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,32 @@
+from django.http import HttpResponse
+from django.core import serializers
+
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
-from django.shortcuts import render
 
 # Create your views here.
 def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Hayfa Najma'
+    }
     return render(request, "wishlist.html", context)
 
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Hayfa Najma'
-}
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    # Jika JSON
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    # Jika XML
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
