686a20abcae75aa08221b1ba0dc071e03bd93774
hayfa_najma <hayfanajmaa22@gmail.com>
2022-10-06 14:34:41 +0700

    selesai lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,12 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist
-from wishlist.views import show_xml
-from wishlist.views import show_json
-from wishlist.views import show_json_by_id
-from wishlist.views import show_xml_by_id
-from wishlist.views import register
-from wishlist.views import login_user
-from wishlist.views import logout_user
+from wishlist.views import *
 
 app_name = 'wishlist'
 
@@ -15,8 +8,9 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
-    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('ajax/', wishlist_ajax, name='wishlist_ajax'),
+    path('ajax/submit/', add_wishlist, name='add_wishlist')
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,16 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from wishlist.models import BarangWishlist
-from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-import datetime
-from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
+import datetime
 
 # Create your views here.
 @login_required(login_url='/wishlist/login/')
@@ -22,6 +20,27 @@ def show_wishlist(request):
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
+
+@login_required(login_url='/wishlist/login/')
+def wishlist_ajax(request):
+    context = {
+        'nama': 'Hayfa Najma',
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "wishlist_ajax.html", context)
+
+@login_required(login_url='/wishlist/login/')
+@csrf_exempt
+def add_wishlist(req):
+    if req.method == "POST":
+        nama = req.POST.get("nama")
+        harga = req.POST.get("harga")
+        deskripsi = req.POST.get("deskripsi")
+        BarangWishlist.objects.create(nama_barang=nama, harga_barang=harga, deskripsi=deskripsi)
+        return HttpResponse()
+    else:
+        print("here")
+        return redirect("wishlist:show_wishlist")
 
 def show_xml(request):
     data = BarangWishlist.objects.all()
@@ -35,11 +54,6 @@ def show_json_by_id(request, id):
     data = BarangWishlist.objects.filter(pk=id)
     # Jika JSON
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-def show_xml_by_id(request, id):
-    data = BarangWishlist.objects.filter(pk=id)
-    # Jika XML
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def register(request):
     form = UserCreationForm()
